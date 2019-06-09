9f913543308e67ec9610fe16fcfcc80247b765b6
edge2g <maira.cristian54@gmail.com>
2019-06-09 13:13:35 -0400

    Primer commit

---
+++ application/views/crud_view.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Administracion</title>
+  </head>
+  <body>
+	<!-- As a link -->
+	<nav class="navbar navbar-dark bg-dark">
+	  <a class="navbar-brand" href="#">Menu de administracion</a>
+	</nav>
+
+	<div class="container">
+		<br>
+		<table class="table">
+  			<thead>
+    			<tr>
+      				<th scope="col">id</th>
+					<th scope="col">Nombre</th>
+					<th scope="col">Contraseña</th>
+					<th scope="col">Mail</th>
+    			</tr>
+  			</thead>
+  			<tbody>
+    			<tr>
+					<th scope="row">1</th>
+					<td>Mark</td>
+					<td>Otto</td>
+					<td>@mdo</td>
+    			</tr>
+ 			 </tbody>
+		</table>
+	</div>
+
+	<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
